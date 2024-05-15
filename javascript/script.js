f326ef14adf68bd4ed54d9478c76ed6a4ce32ee0
Colm <colm.murphy5@gmail.com>
2024-05-15 12:04:46 +0100

    nav button working and added to main pages + JS link

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const navBtn = document.querySelector('.nav-btn');
-    const navbar = document.querySelector('.navbar');
     const navTabs = document.querySelector('.nav-tabs');
 
+    // on click
     navBtn.addEventListener('click', function() {
-        if (navTabs.style.display === 'none') {
-            navTabs.style.display = 'flex';
-        }
-        else {
-            navTabs.style.display = 'none';
+        // check if the nav tabs are being shown
+        if (navBtn.classList.contains('active')) {
+            // if so, hide them and remove 'active' class
+            navTabs.style.transform = 'translateX(150%)';
+            navBtn.classList.remove('active');
+        } else {
+            //if they arent being shown, show them and add the 'active' class
+            navTabs.style.transform = 'translateX(2.5%)';
+            navBtn.classList.add('active');
         }
     });
 });
